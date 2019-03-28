74881474d82cb89946e782f60940db327b844c2d
kylelambert <kylelambertdev@gmail.com>
2019-03-28 14:27:06 -0400

    updated shader loading

---
+++ getShader.js
@@ -0,0 +1,49 @@
+
+var shaderText;
+
+function getShader(gl, id) {
+    var shaderScript = document.getElementById(id);
+    if (!shaderScript) {
+        return null;
+    }
+
+    var str = "";
+    var k = shaderScript.getAttribute('src');
+    readTextFile(k);
+
+    var shader;
+    if (shaderScript.type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        return null;
+    }
+
+    gl.shaderSource(shader, shaderText);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(gl.getShaderInfoLog(shader));
+        return null;
+    }
+
+    return shader;
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                shaderText = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
--- main.js
+++ main.js
@@ -1,77 +1,67 @@
 
-var canvas, gl, vertexShader, fragmentShader, program;
+var canvas, gl, program;
 
 // start() is the main function that gets called first by index.html
 var start = function() {
+	initCanvas();
     initShaders();
-    initCanvas();
-
-};
-
-// loads the shader text and calls createShaders to initialize shaders
-var initShaders = function() {
-    loadTextResource('./Shaders/vertexShader.glsl', function (vertexError, vertexText) {
-		if (vertexError) {
-			alert('Fatal error getting vertex shader (see console)');
-			console.error(vertexError);
-		} else {
-			loadTextResource('./Shaders/fragmentShader.glsl', function (fragmentError, fragmentText) {
-				if (fragmentError) {
-					alert('Fatal error getting fragment shader (see console)');
-					console.error(fragmentError);
-				} else {
-                    createShaders(vertexText, fragmentText);
-				}
-			});
-		}
-	});
-};
-
-// compiles shaders and creates program
-var createShaders = function(vertexText, fragmentText) {
-    vertexShader = gl.createShader(gl.VERTEX_SHADER);
-	fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-	gl.shaderSource(vertexShader, vertexText);
-	gl.shaderSource(fragmentShader, fragmentText);
-
-	gl.compileShader(vertexShader);
-	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-		console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
-		return;
-	}
-
-	gl.compileShader(fragmentShader);
-	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-		console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
-		return;
-    }
-
-    program = gl.createProgram();
-	gl.attachShader(program, vertexShader);
-	gl.attachShader(program, fragmentShader);
-	gl.linkProgram(program);
-	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-		console.error('ERROR linking program!', gl.getProgramInfoLog(program));
-		return;
-    }
     
-	gl.validateProgram(program);
-	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-		console.error('ERROR validating program!', gl.getProgramInfoLog(program));
-		return;
-	}
-}
+    requestAnimationFrame(animate);
+};
 
-// starts the canvas
-var initCanvas = function(vertexText, fragmentText) {
+// starts the canvas and gl
+var initCanvas = function() {
 	canvas = document.getElementById('game-surface');
     gl = canvas.getContext('webgl2');   // WebGL 2
-    
+
     gl.clearColor(0.53, 0.81, 0.92, 1.0);   // sky blue
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	gl.enable(gl.DEPTH_TEST);
 	gl.enable(gl.CULL_FACE);
 	gl.frontFace(gl.CCW);
     gl.cullFace(gl.BACK);  
+}
+
+// initializes the shaders
+var initShaders = function() {
+
+    program = gl.createProgram();
+
+	var vertexShader = getShader(gl, "vertShader");
+    var fragmentShader = getShader(gl, "fragShader");
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        alert("Could not initialise shaders");
+    }
+
+    gl.useProgram(program);
+}
+
+// animation loop
+var animate = function() {
+
+    resize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0.53, 0.81, 0.92, 1.0);   // sky blue
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    requestAnimationFrame(animate);
+}
+
+// resizes canvas to fit browser window
+var resize = function(canvas) {
+    // Lookup the size the browser is displaying the canvas.
+    var displayWidth  = canvas.clientWidth;
+    var displayHeight = canvas.clientHeight;
+
+    // Check if the canvas is not the same size.
+    if (canvas.width  !== displayWidth || canvas.height !== displayHeight) {
+        // Make the canvas the same size
+        canvas.width  = displayWidth;
+        canvas.height = displayHeight;
+    }
 }
