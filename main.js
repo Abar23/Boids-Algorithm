1e9a86700c4f518c099ee50736c7670bc31bd154
Anthony Bartholomew <abar15.abb@gmail.com>
2020-02-05 21:55:20 -0500

    Added README.md and adjusted number of boids

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ var start = function() {
     program = new Shader('vertShader', 'fragShader');
     program.UseProgram();
 
-    flock = new Flock(100, program);
+    flock = new Flock(25, program);
     cloudBatch = new CloudBatch(30, program);
 
     drawScene();
